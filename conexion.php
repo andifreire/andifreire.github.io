28ad2c64b1117d0213ecd4a60d90403d603ce346
andifreire <70733584+andifreire@users.noreply.github.com>
2022-07-04 19:56:27 -0300

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+include("configuracion.php");
+$conexion= new mysqli($server,$user,$pass,$bd);
+if (mysqli_connect_errno()){
+	echo "NO CONECTADO" ,  mysqli_connect_error();
+	exit();
+}
+else {echo"";}
+?>
